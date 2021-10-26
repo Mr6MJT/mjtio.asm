a1c348797b0c5c9066174109e4d2d2b97d130027
Mahdi <87242348+Mr6MJT@users.noreply.github.com>
2021-10-26 15:38:41 +0300

    Add files via upload

---
+++ mjtio.asm
@@ -0,0 +1,42 @@
+; #############################################################################################################
+; hello guys i am Mr MJT,this file is coded by me,include this file into your file by typing %include "mjt.asm"
+; ##############################################################################################################
+; https://github.com/Mr6MJT
+
+
+; this file will help the programmers that learned assembly language
+
+
+; print <textname>, <length of the string>
+; input <TheNameThatYouTypedIn.bss>, <bytes>
+; exit
+%macro exit 0
+	mov rax, 60
+	mov rdi, 0
+	syscall
+%endmacro
+
+%macro print 2
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, %1
+	mov rdx, %2
+	syscall
+%endmacro
+
+
+%macro input 2
+	mov rax, 0
+	mov rdi, 0
+	mov rsi, %1
+	mov rdx, %2
+	syscall
+%endmacro
+
+%macro prInput 2
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, %1
+	mov rdx, %2
+	syscall
+%endmacro
